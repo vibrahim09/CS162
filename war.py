36acbe42d60d8bcb3f1e1c3c7d6afb838dd0ade9
Ibrahim <vibrahim09@gmail.com>
2023-01-28 09:56:22 -0800

    Random seed added. Modified display_hand()

--- war.py
+++ war.py
@@ -4,7 +4,9 @@
 [extended_summary]
 """
 from deck import Deck
+import random
 
+random.seed(100)
 # Create the deck of cards and display it.
 the_deck = Deck()
 the_deck.shuffle()
@@ -21,32 +23,34 @@ for i in range(26):
         player2_hand.append(the_deck.deal())
 
 
-print(len(player1_hand))
-print(len(player2_hand))
-
-def display_hand(player_hand: list, cols: int = 13) -> None:
+def display_hand(player_hand: list, player_number: int, cols: int = 13) -> None:
     """
         Display the player hand in a readable format.
+
         args:
-        player_list: a List that includes the players cards
+            player_list (list): a List that includes the players cards.
+            player_number (int): The number of the player.
+            cols (int, optional): The amount of columns to display the players hand,
+                defaults to 13.
+
+        
 
     """
-
+    print(f"Player {player_number} hand: ", end="")
     for index, card in enumerate(player_hand):
         if not index % cols:
             print()
         print(f"{card!s:4}", end="")
     print("\n" * 2)
 
-for index, card in enumerate(player2_hand):
-    if not index % 13:
-        print()
-    print(f"{card!s:4}", end="")
+display_hand(player1_hand, 1)
+
+display_hand(player2_hand, 2)
 
 print("\n" * 2)
 
-card1 = player1_hand.pop()
-card2 = player2_hand.pop()
+card1 = player1_hand.pop(0)
+card2 = player2_hand.pop(0)
 print(card1)
 print(card2)
 
