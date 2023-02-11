8cefe0bc6a5b139e1b0778e14e4c6bfd1617f330
Ibrahim Velasquez <vibrahim09@gmail.com>
2023-02-11 00:16:44 -0800

    Created venv and added shape classes

---
+++ virtual_envir/shape.py
@@ -0,0 +1,18 @@
+class Shape():
+    def __init__(self):
+        pass
+    def area(self):
+        return float
+    def create(self):
+        return Shape
+    def contains(self, other):
+        return bool
+    def overlaps(self, other):
+        return bool
+    def __str__(self):
+        return str
+    def __repr__(self):
+        return str
+    
+    
+    
