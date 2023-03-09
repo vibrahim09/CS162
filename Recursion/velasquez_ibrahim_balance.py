6024b7f94b3410496b6305631b080b998ddd05f0
Ibrahim Velasquez <vibrahim09@gmail.com>
2023-03-08 23:12:14 -0800

    added balance.py

--- Recursion/velasquez_ibrahim_balance.py
+++ Recursion/velasquez_ibrahim_balance.py
@@ -4,8 +4,9 @@
 def balance(string: str) -> bool:
     if string.startswith("(") and string.endswith(")"):
         return "Balance parenthesis"
+    return False
 
 
 if __name__ == "__main__":
-    string = "()"
+    string = "()()("
     print(balance(string))
